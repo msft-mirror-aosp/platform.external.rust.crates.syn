218e269f27cf34c525b7acc48614d69fe0ad1f74
Chih-Hung Hsieh <chh@google.com>
2020-03-20 13:02:10 -0700

    Update to latest syn-1.0.16
    * fill back missing NOTICE,METADATA,*LICENSE* files
    
    Bug: 150877376
    Test: make
    Test: atest --host -c --include-subdirs external/rust/crates
    Change-Id: Ib5df6b8fb97764214e701a888e44fab3a4245800

--- tests/test_round_trip.rs
+++ tests/test_round_trip.rs
@@ -2,19 +2,23 @@
 #![recursion_limit = "1024"]
 #![feature(rustc_private)]
 
+extern crate rustc_errors;
+extern crate rustc_expand;
+extern crate rustc_parse as parse;
+extern crate rustc_session;
+extern crate rustc_span;
 extern crate syntax;
-extern crate syntax_pos;
 
 mod features;
 
 use quote::quote;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use rustc_errors::PResult;
+use rustc_session::parse::ParseSess;
+use rustc_span::edition::Edition;
+use rustc_span::source_map::FilePathMapping;
+use rustc_span::FileName;
 use syntax::ast;
-use syntax::parse::{self, PResult};
-use syntax::sess::ParseSess;
-use syntax::source_map::FilePathMapping;
-use syntax_pos::edition::Edition;
-use syntax_pos::FileName;
 use walkdir::{DirEntry, WalkDir};
 
 use std::fs::File;
@@ -35,7 +39,6 @@ mod repo;
 use common::eq::SpanlessEq;
 
 #[test]
-#[cfg_attr(target_os = "windows", ignore = "requires nix .sh")]
 fn test_round_trip() {
     repo::clone_rust();
     let abort_after = common::abort_after();
